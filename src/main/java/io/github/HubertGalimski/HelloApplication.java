dda5c9c4648dad7cdd74c8b9f746024625de71bd
hg <hubertuber5@gmail.com>
2019-10-15 15:23:23 +0200

    Added swagger

--- src/main/java/io/github/HubertGalimski/HelloApplication.java
+++ src/main/java/io/github/HubertGalimski/HelloApplication.java
@@ -2,7 +2,16 @@ package io.github.HubertGalimski;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
+@Configuration
 @SpringBootApplication
 public class HelloApplication {
 
@@ -10,5 +19,13 @@ public class HelloApplication {
 		SpringApplication.run(HelloApplication.class, args);
 	}
 
+	@Bean
+	public Docket docket() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
 
